ab84ffdb6c1db7bfadc4caaa8f746eb4d9833766
ImtiazHabib <imtiazhabib7@gmail.com>
2025-05-15 10:20:55 +0600

    custom post type -- 21.(1-4)-- add a plugin of cpti

--- about.php
+++ about.php
@@ -1,52 +1,52 @@
-<?php 
+<?php
 /**
  *  Template Name: About Us Template
  */
 the_post();
-  get_header();
+get_header();
 ?>
 
-    <!-- s-content
+<!-- s-content
     ================================================== -->
-    <section class="s-content s-content--narrow s-content--no-padding-bottom">
+<section class="s-content s-content--narrow s-content--no-padding-bottom">
 
-        <article class="row format-standard">
+    <article class="row format-standard">
 
-            <div class="s-content__header col-full">
-                <h1 class="s-content__header-title">
-                    <?php echo the_title(); ?>
-                </h1>
-            </div> <!-- end s-content__header -->
-    
-            <div class="s-content__media col-full">
-                <div class="s-content__post-thumb">
-                    <?php 
-                    echo the_post_thumbnail("large" );
-                    ?>
-                </div>
-            </div> <!-- end s-content__media -->
+        <div class="s-content__header col-full">
+            <h1 class="s-content__header-title">
+                <?php echo the_title(); ?>
+            </h1>
+        </div> <!-- end s-content__header -->
 
-            <div class="col-full s-content__main">
-
-                <?php 
-                echo the_content();
+        <div class="s-content__media col-full">
+            <div class="s-content__post-thumb">
+                <?php
+                echo the_post_thumbnail("large");
                 ?>
-                <div class="row block-1-2 block-tab-full">
-                    
-                <?php  
-                  if(is_active_sidebar("about-us-sidebar")){
-                     dynamic_sidebar('about-us-sidebar'); 
-                  }
+            </div>
+        </div> <!-- end s-content__media -->
+
+        <div class="col-full s-content__main">
+
+            <?php
+            echo the_content();
+            ?>
+            <div class="row block-1-2 block-tab-full">
+
+                <?php
+                if (is_active_sidebar("about-us-sidebar")) {
+                    dynamic_sidebar('about-us-sidebar');
+                }
                 ?>
-                    
-                </div>
-            </div> <!-- end s-content__main -->
 
-        </article>
-    </section> <!-- s-content -->
+            </div>
+        </div> <!-- end s-content__main -->
 
-    <?php 
-      
-      get_footer();
+    </article>
+</section> <!-- s-content -->
 
-  ?>
+<?php
+
+get_footer();
+
+?>
